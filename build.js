f02fa628b0455c4a87a6ab3e423886f56f2ce5b6
Pawel Psztyc <jarrodek@gmail.com>
2018-03-22 11:35:44 -0700

    Updated script command

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
 const builder = require('api-console-builder');
 
 builder({
-  src: 'https://github.com/mulesoft/api-console/archive/release/4.0.0.zip',
+  src: '../api-console-4.2.1.zip',
   dest: 'build',
-  raml: 'https://cdn.rawgit.com/advanced-rest-client/drive-raml-api-v2/1f85d308/api.raml',
+  // raml: 'https://cdn.rawgit.com/advanced-rest-client/drive-raml-api-v2/1f85d308/api.raml',
+  raml: '../drive-raml-api-v2/api.raml',
   verbose: true,
   useJson: true,
   inlineJson: true,
-  noOptimization: true
+  noOptimization: false,
+  sourceIsZip: true
 })
 .then(() => console.log('Build complete'))
 .catch((cause) => console.log('Build error', cause.message));
