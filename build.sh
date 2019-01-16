6d91a1b1eebb9eda299753d3bbf4daa526f186ba
Pawel <jarrodek@gmail.com>
2019-01-15 21:44:29 -0800

    Updating demo page

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-api-console build https://cdn.rawgit.com/advanced-rest-client/drive-raml-api-v2/1f85d308/api.raml --json --inline-json --verbose
+api-console build -t "RAML 1.0" -a drive-raml-api-v2-master/api.raml --verbose
 cp -R build/. .
 rm -rf build/
---
+++ drive-raml-api-v2-master/README.md
@@ -0,0 +1,3 @@
+## Google Drive API v2 as RAML spec
+
+This is an API spec to test RAML components.
