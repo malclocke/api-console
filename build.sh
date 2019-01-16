573fb967a54812e232e7fcb8752dbe945e20dcad
Pawel <jarrodek@gmail.com>
2019-01-15 22:08:02 -0800

    Updating demo page

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-api-console build -t "RAML 1.0" -a drive-raml-api-v2-master/api.raml --verbose
+NODE_OPTIONS=--max_old_space_size=2048 api-console build -t "RAML 1.0" -a drive-raml-api-v2-master/api.raml --verbose
 cp -R build/. .
 rm -rf build/
